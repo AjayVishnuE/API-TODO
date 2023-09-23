310e46ec77d0c96b0b574abee48198b99cbe30aa
Ajay Vishnu E <ajayvishnu121@gmail.com>
2023-09-23 21:52:41 +0530

    commit

--- API_ToDo/wsgi.py
+++ API_ToDo/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'API_ToDo.settings')
 
 application = get_wsgi_application()
+
+app = application              #for hosting in vercel
