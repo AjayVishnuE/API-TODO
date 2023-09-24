660ccbdd4bca19eefcfd7e5c69c0ca5e124a9c1e
Ajay Vishnu E <ajayvishnu121@gmail.com>
2023-09-24 19:47:48 +0530

    deploy

--- API_ToDo/settings.py
+++ API_ToDo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-^ufz-b=##n494v%rr8#d8^=y!kbt6t+nk8$3l6wd#u((-w35sq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://todoappapi.up.railway.app/']
+ALLOWED_HOSTS = ['https://todoappapi.up.railway.app/','todoappapi.up.railway.app','http://todoappapi.up.railway.app']
 
 
 # Application definition
