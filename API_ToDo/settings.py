ee83c54c45a4765d3a9d31893445d137f6fd5a30
Ajay Vishnu E <ajayvishnu121@gmail.com>
2023-09-24 19:45:21 +0530

    deploy

--- API_ToDo/settings.py
+++ API_ToDo/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^ufz-b=##n494v%rr8#d8^=y!kbt6t+nk8$3l6wd#u((-w35sq'
     
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://todoappapi.up.railway.app/']
 
